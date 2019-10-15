45e258f63e54abc11248ae5ad5ac1b468c239d54
Tom S <tom_wrk@outlook.com>
2019-10-15 14:46:19 -0700

    Updated ApiController.php

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Lib\CurrencyValidator;
 use App\Lib\Denomination;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,29 +15,52 @@ class ApiController extends AbstractController
   /**
    * Controller method for the Cash Register API
    *
-   * @param Request $symfonyRequest Request object
-   *     Example; $symfonyRequest->getRequestUri() =
+   * @param Request $request Request object
+   *     Example; $request->getRequestUri() =
    *         http://localhost:8000/api/cash_register/?total_cost=87.66&amount_provided=100.00
    * @return Response Response object
    * @throws \Exception
    * @Route("/api/cash_register/", name="cash_register", requirements={"params"=".+"})
    */
-  public function cash_register(Request $symfonyRequest): Response
+  public function cash_register(Request $request): Response
   {
-    $totalCost = self::getValidatedCurrency('total_cost', $symfonyRequest);
-    $amountProvided = self::getValidatedCurrency('amount_provided', $symfonyRequest);
-    $change = $amountProvided - $totalCost;
-    $currencyRepository = $this->getDoctrine()->getRepository(\App\Entity\Currency::class);
-    $aCurrency=$currencyRepository->findBy(['is_active'=>true],['amount'=>'DESC']);
-    $changeDenominations = Denomination::getDenominations($change,$aCurrency);
+    //Obtain return values
+    $change = self::getChange($request);
+    $changeDenominations = self::getDenominations($change, $this->getDoctrine());
     $arrReturnValues = [
-      'change' => round($change,2),
+      'change' => round($change, 2),
       'denominations' => $changeDenominations
     ];
-    $response  = new Response();
-    $response ->setContent(json_encode($arrReturnValues));
+    //Response
+    $response = new Response();
+    $response->setContent(json_encode($arrReturnValues));
     $response->headers->set('Content-Type', 'application/json');
     return $response;
+  }
+
+  /**
+   * Obtain Change corresponding given parametes
+   * @param Request $request Request object
+   * @return float Change
+   * @throws \Exception
+   */
+  private static function getChange(Request $request): float
+  {
+    $totalCost = self::getValidatedCurrency('total_cost', $request);
+    $amountProvided = self::getValidatedCurrency('amount_provided', $request);
+    return $amountProvided - $totalCost;
+  }
+
+  /**
+   * Obtains an array of count and type of currency corresponding to given dollar amount.
+   * @param float $amount Amount
+   * @param ManagerRegistry $managerRegistry ManagerRegistry object
+   * @return array An array of count and currency
+   */
+  private static function getDenominations(float $amount, ManagerRegistry $managerRegistry): array{
+    $currencyRepository = $managerRegistry->getRepository(\App\Entity\Currency::class);
+    $aCurrency = $currencyRepository->findBy(['is_active' => true], ['amount' => 'DESC']);
+    return Denomination::getDenominations($amount, $aCurrency);
   }
 
   /**
@@ -50,11 +74,12 @@ class ApiController extends AbstractController
    *     Example: 87.66
    * @throws \Exception Throws an exception if the given currency value is not numeric or exceeds supported precision.
    */
-  private static function getValidatedCurrency(string $paramName, Request $symfonyRequest): float{
-    try{
-      return CurrencyValidator::validate($symfonyRequest->get($paramName));
-    }catch(\Exception $e){
-      throw new \Exception("Error on value for paramName = '".$paramName."':  ".$e->getMessage());
+  private static function getValidatedCurrency(string $paramName, Request $request): float
+  {
+    try {
+      return CurrencyValidator::validate($request->get($paramName));
+    } catch (\Exception $e) {
+      throw new \Exception("Error on value for paramName = '" . $paramName . "':  " . $e->getMessage());
     }
   }
 
