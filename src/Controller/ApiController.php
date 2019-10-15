54442eb7ff90159c8ba34e32316ab4ee986ea859
Tom S <tom_wrk@outlook.com>
2019-10-15 08:12:06 -0700

    Updated ApiController.php

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Lib\Denomination;
+use mysql_xdevapi\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,21 +11,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
 {
-  /**
-   * Example;
-   * http://localhost:8000/api/cash_register/8766/10000/
-   * @Route("/api/cash_register/{total_cost}/{amount_provided}/", name="cash_register", requirements={"params"=".+"})
+  /** Controller method for the Cash Register API
+   *
+   * @param Request $symfonyRequest Request object
+   *     Example; $symfonyRequest->getRequestUri() =
+   *         http://localhost:8000/api/cash_register/?total_cost=87.66&amount_provided=100.00
+   * @return Response Response object
+   * @throws \Exception
+   * @Route("/api/cash_register/", name="cash_register", requirements={"params"=".+"})
    */
   public function cash_register(Request $symfonyRequest): Response
   {
-    $totalCostCent = (int)$symfonyRequest->get('total_cost');
-    $amountProvidedCent = (int)$symfonyRequest->get('amount_provided');
-    $changeDollar = (float)($amountProvidedCent - $totalCostCent)/100;
+    $totalCostCent = self::getValidatedCurrency('total_cost', $symfonyRequest);
+    $amountProvidedCent = self::getValidatedCurrency('amount_provided', $symfonyRequest);
+    $changeDollar = $amountProvidedCent - $totalCostCent;
     $currencyRepository = $this->getDoctrine()->getRepository(\App\Entity\Currency::class);
     $aCurrency=$currencyRepository->findBy(['is_active'=>true],['amount'=>'DESC']);
     $changeDenominations = Denomination::getDenominations($changeDollar,$aCurrency);
     $arrReturnValues = [
-      'change' => $changeDollar,
+      'change' => round($changeDollar,2),
       'denominations' => $changeDenominations
     ];
     $symfonyResponse = new Response(
@@ -34,4 +39,33 @@ class ApiController extends AbstractController
     $symfonyResponse->setContent(json_encode($arrReturnValues));
     return $symfonyResponse;
   }
+
+  /**
+   * Obtain currency value corresponding to given param name and Request object.
+   * @param string $paramName Param name
+   *     Example: total_cost
+   * @param Request $symfonyRequest Request object
+   *     Example; $symfonyRequest->getRequestUri() =
+   *         http://localhost:8000/api/cash_register/?total_cost=87.66&amount_provided=100.00
+   * @return float currency value
+   *     Example: 87.66
+   * @throws \Exception Throws an exception if the given currency value is not numeric or exceeds supported precision.
+   */
+  private static function getValidatedCurrency(string $paramName, Request $symfonyRequest): float{
+    $sCurrencyValue = $symfonyRequest->get($paramName);
+    $aNodeCurrencyValue = explode('.',$sCurrencyValue);
+    if(isset($aNodeCurrencyValue[1])){
+      $sAfterDecimalPoint=$aNodeCurrencyValue[1];
+    }else{
+      $sAfterDecimalPoint = '0';
+    }
+    if(!is_numeric($sCurrencyValue )){
+      throw new \Exception('Given value for '.$paramName.' = '.$sCurrencyValue.' is not numeric');
+    }elseif(strlen($sAfterDecimalPoint)>2){
+      throw new \Exception('Precision for the given value for '.$paramName.' = '.$sCurrencyValue.' is supported.');
+    }else{
+      return (float)$sCurrencyValue;
+    }
+  }
+
 }
