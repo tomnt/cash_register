8e76895b85fd22c72481c5702aee74be0bbf64bd
Tom S <tom_wrk@outlook.com>
2019-10-22 12:58:44 -0700

    Cleaned CurrencyRepository.php

--- src/Repository/CurrencyRepository.php
+++ src/Repository/CurrencyRepository.php
@@ -14,37 +14,8 @@ use Doctrine\Common\Persistence\ManagerRegistry;
  */
 class CurrencyRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Currency::class);
-    }
-
-    // /**
-    //  * @return Currency[] Returns an array of Currency objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Currency
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+  public function __construct(ManagerRegistry $registry)
+  {
+    parent::__construct($registry, Currency::class);
+  }
 }
